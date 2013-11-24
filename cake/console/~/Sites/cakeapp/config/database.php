e81d1eccf19c96c4f3aa98d089f3b03f41bb90ef
sandeepan <sandeepan.kundu@gmail.com>
2013-11-24 18:30:06 +0530

    Initialization entire folder

---
+++ cake/console/~/Sites/cakeapp/config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $cake_app = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'cakeapp',
+	);
+}
+?>
